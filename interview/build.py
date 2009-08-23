8b7ed6e3e0ac6f2133d17a6b7f71327fd41d9187
Johan Brinch <zerrez@gmail.com>
2009-08-23 05:47:08 +0200

    works with sanitazing for safety

--- interview/build.py
+++ interview/build.py
@@ -2,11 +2,15 @@
 import sys, os, re
 PROCESS_DIR = r'processing/'
 LATEX = r'\\includegraphics[height=\\textwidth]{images/%s.eps}\n'
-print LATEX
 tmpl  = file('template.tex').read()
-CMDS  = ['convert images/%(name)s images/%(name)s.eps', 'latex processing/%(name)s',
-         'dvips %(name)s', 'ps2pdf %(name)s.ps', 'rm -f ./%(name)s.*'] #, 'rm -f ./%(name)s.*']
+CMDS  = ['convert "images/%(name)s" "images/%(name)s.eps"', 'latex "processing/%(name)s"',
+         'dvips "%(name)s"', 'ps2pdf "%(name)s.ps"', 'mv "%(name)s.pdf" "compiled/"',
+         'rm -fv "./%(name)s."* "./processing/%(name)s."* "./images/%(name)s.eps"']
 for fname in os.listdir('./images'):
+    if not re.match(r'^[\w\.]+$', fname): # sanitize
+        print '!! SKIPPING WEIRD FILENAME: "%s"' % fname
+        print '!! PRESS ANY KEY TO CONTINUE'
+        sys.stdin.readline()
+        continue
     file('%s/%s.tex' % (PROCESS_DIR, fname), 'w').write(re.sub('%+\s*BILLEDE', LATEX % fname, tmpl))
-    # run cmds
     map(lambda cmd: os.system(cmd % {'name': fname}), CMDS)
